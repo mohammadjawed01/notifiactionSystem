d33d7f12d26aeb45959f2773cebb8512e13470e2
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-09-10 22:10:16 +0530

    Enhance createToaster function to support dynamic positioning and automatic removal of notifications and add conditions so they run different ways and add setTimeout so they  go after one message complete

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ function createToaster(config) {
         let div = document.createElement("div");
         div.textContent = str;
         div.className = `inline-block ${config.theme === "dark" ? "bg-gray-800 text-white" : "bg-gray-100 text-black"} px-6 py-3 rounded shadow-lg pointer-events-none`;
+
+        document.querySelector(".parent").appendChild(div);
+
+        if (config.positionX !== "left" && config.positionX !== "top") {
+            document.querySelector(".parent").className += `${config.positionX === "right" ? " right-5" : " left-5"} ${config.positionY === "bottom" ? " bottom-5" : " top-5"}`
+        }
+
+        setTimeout(() => {
+            document.querySelector(".parent").removeChild(div);
+        }, config.duration * 1000);
     };
 }
 
@@ -15,6 +25,9 @@ let toaster = createToaster({
 })
 
 toaster("Download Completed!");
+setTimeout(() => {
+    toaster("don't accept the request!");
+}, 2000)
 
 
 
