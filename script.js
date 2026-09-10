b7a4a1d51a9e5dcb722002e1361333916601c9aa
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-09-10 21:52:19 +0530

    create a new child div in js and add style and add classes to the child div

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function createToaster(config) {
     return function (str) {
-        
+        let div = document.createElement("div");
+        div.textContent = str;
+        div.className = `inline-block ${config.theme === "dark" ? "bg-gray-800 text-white" : "bg-gray-100 text-black"} px-6 py-3 rounded shadow-lg pointer-events-none`;
+    };
 }
+
+
+let toaster = createToaster({
+    positionX: "right",
+    positionY: "bottom",
+    theme: "light",
+    duration: 3,
+})
+
+toaster("Download Completed!");
+
+
 
